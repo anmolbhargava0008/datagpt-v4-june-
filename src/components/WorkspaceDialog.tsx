253cb7c7166cb9106b6c494767593227db253ce3
anmol008 <anmolbhargava97@gmail.com>
2025-08-13 14:55:08 +0530

    old workspace name bug fix

--- src/components/WorkspaceDialog.tsx
+++ src/components/WorkspaceDialog.tsx
@@ -29,7 +29,7 @@ const WorkspaceDialog = ({ isOpen, onClose, workspace }: WorkspaceDialogProps) =
     } else {
       setWorkspaceName('');
     }
-  }, [workspace]);
+  }, [workspace, isOpen]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
